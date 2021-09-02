5bcc7c4a174dbdc1bd7748bf07d05c144eb4b586
Md. Nagib Mahfuz Subho <nmsubho@gmail.com>
2021-09-02 08:51:27 +0600

    alternatives

--- JS/script.js
+++ JS/script.js
@@ -19,18 +19,19 @@ const displaySearchResult = books => {
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
 
+    console.log(books.length)
     books.forEach(element => {
         console.log(element);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
         <div class="card h-100">
-            <img src="https://covers.openlibrary.org/b/id/${element.cover_i}-M.jpg" class="card-img-top" alt="...">
+            <img src="https://covers.openlibrary.org/b/id/${element.cover_i}-M.jpg" class="card-img-top" alt="No Image">
             <div class="card-body">
                 <h5 class="card-title">${element.title}</h5>
-                <p class="card-text">Author: ${element.author_name}</p>
-                <p class="card-text">Publisher: ${element.publisher}</p>
-                <p class="card-text">First Published: ${element.first_publish_year}</p>
+                <p class="card-text">Author: ${element.author_name ? element.author_name : 'Not Specified'}</p>
+                <p class="card-text">Publisher: ${element.publisher ? element.publisher : 'Not Specified'}</p>
+                <p class="card-text">First Published: ${element.first_publish_year ? element.first_publish_year : 'Not Specified'}</p>
             </div>               
         </div>`;
         searchResult.appendChild(div);
